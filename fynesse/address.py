f39caa2ef189ab1e8f68b296c5688460cd5cd0f6
Paul-Bett <104768031+Paul-Bett@users.noreply.github.com>
2025-09-23 03:04:07 +0300

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -9,7 +9,7 @@ This module handles question addressing functionality including:
 """
 
 from typing import Any, Union
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 import pandas as pd
 import logging
 import numpy as np
